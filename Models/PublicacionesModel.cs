c4f5953582939411812bbab9ab38ef96c47ec6f5
MazzCode <mazzcode@gmail.com>
2022-07-26 17:02:01 -0300

    Models
    Listo ambos modelos

---
+++ Models/PublicacionesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InstaMazz3.Models
+{
+    public class PublicacionesModel
+    {
+        public int IdPost { get; set; }
+        public int IdUsuario { get; set; }
+        public string UrlImg { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿namespace InstaMazz3.Models
+{
+    public class UsuarioModel
+    {
+        public int IdUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public string email { get; set; }
+
+        public byte ImagenPerfil { get; set; }
+
+        public string ConfirmarClave { get; set; }
+
+        public string imagenPerf { get; set; }
+    }
+}
