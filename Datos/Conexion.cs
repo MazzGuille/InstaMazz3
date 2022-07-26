a67edc349cb1275c543431ab510a8413b9059cb4
MazzCode <mazzcode@gmail.com>
2022-07-26 16:50:11 -0300

    Data.Cs
    Creada la variable para la cadena de conexion

---
+++ Datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿namespace InstaMazz3.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSql = string.Empty;
+
+        public Conexion()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            cadenaSql = builder.GetSection("ConnectionStrings:CadenaSql").Value;
+        }
+
+        public string GetCadenaSql()
+        {
+            return cadenaSql;
+        }
+
+    }
+}
